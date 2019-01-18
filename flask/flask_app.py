a6f5bcdd889f48ae6a1402b29389d147ab7d5ca8
James Robinson <james@jlr-photo.com>
2019-01-18 12:47:41 -0500

    line length cleanup in flask_app.py.

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -7,17 +7,17 @@ import jlr.db as db
 import factory_model
 
 
-APPLICATION_ROOT = '/api'
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 socketio = SocketIO(app)
 
-db_config = 'dbname=%s user=%s host=%s' % (env['DBNAME'], env['DBUSER'], env['DBHOST'])
+db_config = 'dbname=%s user=%s host=%s' % (
+                    env['DBNAME'], env['DBUSER'], env['DBHOST'])
 
 connection_manager = db.configure_flask_socketio(db_config, register_types=False)
 
+# decorator to catch exceptions and report back to client.
 def exceptions_to_error_emit(func):
     @wraps(func)
     def doit(*args):
@@ -25,7 +25,8 @@ def exceptions_to_error_emit(func):
             func(*args)
         except Exception as e:
             print(e)
-            emit('serverside-error', {'message': 'Sorry! A server-side error happened!'})
+            emit('serverside-error',
+                {'message': 'Sorry! A server-side error happened!'})
 
     return doit
 
@@ -34,18 +35,86 @@ def exceptions_to_error_emit(func):
 @connection_manager.with_transaction
 def new_connection(con):
     # Return all factories in a list ordered by id
-    emit('factories', {'factories': [ f._asdict() for f in factory_model.all_factories(con)]})
+    emit('factories', {'factories': [ f._asdict()
+                        for f in factory_model.all_factories(con)]})
 
+
+@socketio.on('create_factory')
+@exceptions_to_error_emit
+@connection_manager.with_transaction
+def create_factory(con, data):
+
+    # data: {name, min, max, number_count (count of children to create)}
+    # generates children
+    # stores it
+    # broadcasts it
+
+    name, min_num, max_num, number_count = (
+                            data.get('name'), data.get('min_value'),
+                            data.get('max_value'), data.get('number_count'))
+
+    if complain_about_factory_params(name, min_num, max_num, number_count):
+        return
+
+    new_factory = factory_model.create_factory(con, name, min_num,
+                                                    max_num, number_count)
+
+    emit('new_factory', {'factory': new_factory}, broadcast=True)
+
+
+@socketio.on('delete_factory')
+@exceptions_to_error_emit
+@connection_manager.with_transaction
+def delete_factory(con, data):
+    # deletes it
+    # broadcasts deletion event.
+    f_id = data.get('id')
+    if type(f_id) is not int:
+        reply_error('Expected an int')
+        return
+
+    factory_model.delete_factory(con, f_id)
+    emit('factory_deleted', {'id': f_id}, broadcast=True)
+
+@socketio.on('edit_factory')
+@exceptions_to_error_emit
+@connection_manager.with_transaction
+def edit_factory(con, data):
+    # edits it
+    # regenerates children if change to min/max/number count
+    # broadcasts it
+    f_id, new_name, new_low, new_high, number_count = (
+                        data.get('id'), data.get('name'),
+                        data.get('min_value'), data.get('max_value'),
+                        data.get('number_count'))
+
+    if type(f_id) is not int:
+        reply_error('Expected an int')
+
+    if complain_about_factory_params(new_name, new_low, new_high, number_count):
+        return
+
+    updated_factory = factory_model.update_factory(con, f_id,
+                                new_name, new_low, new_high, number_count)
+
+    emit('factory_updated', {'factory': updated_factory}, broadcast=True)
+
+###
+# Utilities here down, plus __main__ hook.
+###
 
 def reply_error(msg):
     emit('serverside-error', {'message': msg})
     print('reply_error: ' + msg)
 
-
 def complain_about_factory_params(name, min_num, max_num, number_count):
-    if type(name) != str or type(min_num) != int or type(max_num) != int or type(number_count) != int:
+    if type(name) != str or type(min_num) != int \
+            or type(max_num) != int or type(number_count) != int:
+
         reply_error('Bad Parameter Types: %s(%s) %s(%s) %s(%s) %s(%s)' % \
-                (name, type(name), min_num, type(min_num), max_num, type(max_num), type(number_count)))
+                (name, type(name), min_num, type(min_num), max_num,
+                                    type(max_num), type(number_count)))
+
         return True # did indeed complain
 
     if len(name) == 0:
@@ -69,61 +138,6 @@ def complain_about_factory_params(name, min_num, max_num, number_count):
 
     # no complaint ...
     return False
-
-
-@socketio.on('create_factory')
-@exceptions_to_error_emit
-@connection_manager.with_transaction
-def create_factory(con, data):
-
-    # data: {name, min, max}
-    # generates children
-    # stores it
-    # broadcasts it
-
-    name, min_num, max_num, number_count = (data.get('name'), data.get('min_value'),
-                                                data.get('max_value'), data.get('number_count'))
-
-    if complain_about_factory_params(name, min_num, max_num, number_count):
-        return
-
-    new_factory = factory_model.create_factory(con, name, min_num, max_num, number_count)
-    emit('new_factory', {'factory': new_factory}, broadcast=True)
-
-
-@socketio.on('delete_factory')
-@exceptions_to_error_emit
-@connection_manager.with_transaction
-def delete_factory(con, data):
-    # deletes it
-    # broadcasts deletion event.
-    f_id = data.get('id')
-    if type(f_id) is not int:
-        reply_error('Expected an int')
-        return
-
-    factory_model.delete_factory(con, f_id)
-    emit('factory_deleted', {'id': f_id}, broadcast=True)
-
-@socketio.on('edit_factory')
-@exceptions_to_error_emit
-@connection_manager.with_transaction
-def edit_factory(con, data):
-    # edits it
-    # regenerates children if change to min/max
-    # broadcasts it
-    f_id, new_name, new_low, new_high, number_count = (data.get('id'), data.get('name'),
-                                            data.get('min_value'), data.get('max_value'),
-                                            data.get('number_count'))
-
-    if type(f_id) is not int:
-        reply_error('Expected an int')
-
-    if complain_about_factory_params(new_name, new_low, new_high, number_count):
-        return
-
-    updated_factory = factory_model.update_factory(con, f_id, new_name, new_low, new_high, number_count)
-    emit('factory_updated', {'factory': updated_factory}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
